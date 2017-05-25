685ab7da57d7167911e317cc9ddceba6c84d9d58
Carlos Eduardo Pavao Aureliano <a17017@bcc.unifal-mg.edu.brgit config --global user.email a17017@bcc.unifal-mg.edu.br>
2017-05-25 19:41:27 -0300

    Adição de função multiplicação

--- calculadora.c
+++ calculadora.c
@@ -9,7 +9,10 @@ int subtrai (int x, int y){
 	//Alteração feita na branch fsubtrai
 	return x-y;
 }
-int multiplica (){}
+int multiplica (int x, int y){
+	//Alteração feita na branch fmultiplicação
+	return x*y;
+}
 int divide (){}
 
 int main(){
@@ -17,6 +20,6 @@ int main(){
 	printf("hello git!\n\n");
 	printf("Soma: %d\n", soma (2,2));
 	printf("Subtração: %d\n", subtrai (2,2));
-
+	printf("Multiplicação: %d\n", multiplica (2,2));
 
 }
