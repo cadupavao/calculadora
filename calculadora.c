71a1ad05e92814fca3f8aaceb8258111f54fde34
Carlos Eduardo Pavao Aureliano <a17017@bcc.unifal-mg.edu.brgit config --global user.email a17017@bcc.unifal-mg.edu.br>
2017-05-25 19:05:39 -0300

    Adição da função soma

--- calculadora.c
+++ calculadora.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int soma (){}
+int soma (int x, int y){
+	//Aleração feita na branch fsoma
+	return x+y;
+}
 int subtrai (){}
 int multiplica (){}
 int divide (){}
 
 int main(){
 
-	printf("hello hit!");
+	printf("hello hit!\n\n");
+	printf("Soma: %d\n", soma (2,2));
 
 
 }
